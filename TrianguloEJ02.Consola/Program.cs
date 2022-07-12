ca6041d77af42296a5236d84a81aaf882ea6e164
matia <matia@MATIFLORES>
2022-07-11 22:26:04 -0300

    Agregar archivos de proyecto.

---
+++ TrianguloEJ02.Consola/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloEJ02.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Ingrese el lado A: ");
+            int ladoA = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el lado B: ");
+            int ladoB = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el lado C: ");
+            int ladoC = int.Parse(Console.ReadLine());
+
+            var trian2 = new Triangulo(ladoA, ladoB, ladoC);
+            if (trian2.Validar())
+            {
+                Console.WriteLine($"El area es: {trian2.GetArea()}");
+                Console.WriteLine($"El perimetro es: {trian2.GetPerimetro()}");
+                Console.WriteLine($"{trian2.QueTipoDeTriangulo()}");
+            }
+            else
+            {
+                Console.WriteLine("Valores mal ingresado, reintente...");
+            }
+
+            var trian1 = new Triangulo();
+            Console.WriteLine($"Area de triangulo con todos sus lados con valor 1: {trian1.GetArea()}");
+            Console.WriteLine($"Perimetro de triangulo con todos sus lados con valor 1: { trian1.GetPerimetro()}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TrianguloEJ02/Triangulo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloEJ02
+{
+    public class Triangulo
+    {
+        
+        private int LadoB;
+        private int LadoC;
+        private int LadoA;
+
+        public Triangulo(int ladoA, int ladoB, int ladoC)
+        {
+            this.LadoA = ladoA;
+            this.LadoB = ladoB;
+            this.LadoC = ladoC;
+        }
+
+        public Triangulo()
+        {
+            LadoA = 1;
+            LadoB = 1;
+            LadoC = 1;
+        }
+        public bool Validar()
+        {
+            return LadoA + LadoB > LadoC &&
+                LadoA + LadoC > LadoB &&
+                LadoB + LadoC > LadoA;
+        }
+
+        public string QueTipoDeTriangulo()
+        {
+            if (Math.Pow(this.LadoA,2)== Math.Pow(this.LadoB, 2)+ Math.Pow(this.LadoC, 2))
+            {
+                return "Es un triangulo Rectangulo";
+            }
+            else if (Math.Pow(this.LadoA, 2) > Math.Pow(this.LadoB, 2) + Math.Pow(this.LadoC, 2))
+            {
+                return "Es un triangulo Obtusangulo";
+            }
+            else if (Math.Pow(this.LadoA, 2) < Math.Pow(this.LadoB, 2) + Math.Pow(this.LadoC, 2))
+            {
+                return "Es un triangulo Acutangulo";
+            }
+            return "Valor mal ingresado";
+        }
+        public double ObtenerSemiPerimetro()
+        {
+            return (LadoA + LadoB + LadoC) / 2;
+        }
+        public double GetArea()
+        {
+            return Math.Sqrt((ObtenerSemiPerimetro() * (ObtenerSemiPerimetro() - LadoA) * (ObtenerSemiPerimetro() - LadoB) * (ObtenerSemiPerimetro() - LadoC)));
+
+        }
+
+        public double GetPerimetro()
+        {
+            return LadoA + LadoB + LadoC;
+        }
+
+    }
+}
